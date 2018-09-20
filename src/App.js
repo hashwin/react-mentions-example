17caaf9bcb51a35eefba307f9e4a2347da29c29d
Ashwin Hegde <ashwin.hegde12@gmail.com>
2018-09-20 14:15:52 +0530

    Implement minimal mentions

--- src/App.js
+++ src/App.js
@@ -1,19 +1,35 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { MentionsInput, Mention } from 'react-mentions';
+
+const users = [
+  {
+    id: 'walter',
+    display: 'Walter White',
+  },
+  {
+    id: 'jesse',
+    display: 'Jesse Pinkman',
+  }
+]
 
 class App extends Component {
+  state = {
+    value: ''
+  }
+
+  handleChange = (e, value) => {
+    this.setState({ value });
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <MentionsInput value={this.state.value} onChange={this.handleChange}>
+        <Mention
+          trigger="@"
+          data={users}
+          renderSuggestion={this.renderUserSuggestion}
+        />
+      </MentionsInput>
     );
   }
 }
